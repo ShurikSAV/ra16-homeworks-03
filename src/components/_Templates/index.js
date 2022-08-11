a636e5f99e75fd74c197470cb73887b560af8072
Александр Савельев <Savelyev@vector-best.ru>
2022-08-11 09:41:23 +0700

    добавил шаблон компонента

---
+++ src/components/_Templates/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+//import PropTypes from 'prop-types';
+import style from './listing.module.css'
+
+export const Listing = ({count}) => {
+	return (
+		<div className={style.body}>
+
+		</div>
+	)
+}
+
+/*
+Listing.propTypes = {
+	count: PropTypes.number
+	}
+*/
