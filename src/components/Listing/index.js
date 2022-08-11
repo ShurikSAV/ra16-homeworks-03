cc7e017bd071a967d65d003f9d403629a27463b4
Александр Савельев <Savelyev@vector-best.ru>
2022-08-11 09:45:22 +0700

    Listing - заглушка

---
+++ src/components/Listing/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+//import PropTypes from 'prop-types';
+import style from './listing.module.css'
+
+export const Listing = () => {
+	return (
+		<div className={style.body}>
+			Listing
+		</div>
+	)
+}
+
+/*
+Listing.propTypes = {
+	count: PropTypes.number
+	}
+*/
